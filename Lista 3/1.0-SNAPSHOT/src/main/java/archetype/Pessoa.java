668e19be60bd987ace25d3bd7f77e31572308a19
Elios630 <78980830+Elios630@users.noreply.github.com>
2023-10-31 10:35:35 -0300

    Add files via upload

---
+++ Lista 3/1.0-SNAPSHOT/src/main/java/archetype/Pessoa.java
@@ -0,0 +1,16 @@
+package archetype;
+
+public class Pessoa {
+    
+    public static void main(String[] args) {
+        System.out.println( dizerOla() );
+    }
+
+    static String dizerOla(){
+        
+        return "Ola";
+        
+    } 
+}
+
+
---
+++ Lista 3/1.0-SNAPSHOT/src/test/java/archetype/PessoaTest.java
@@ -0,0 +1,12 @@
+package archetype;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class PessoaTest {
+    @Test
+    public void shouldAnswerWithTrue(){
+        assertTrue( true );
+    }
+}
