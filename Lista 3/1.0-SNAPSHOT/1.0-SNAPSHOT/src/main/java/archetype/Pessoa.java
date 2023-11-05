34d828d985da27355e929a4df418f53e069fd428
Elios630 <78980830+Elios630@users.noreply.github.com>
2023-11-05 20:10:47 -0300

    Add files via upload

---
+++ Lista 3/1.0-SNAPSHOT/1.0-SNAPSHOT/src/main/java/archetype/Pessoa.java
@@ -0,0 +1,26 @@
+package archetype;
+
+public class Pessoa {
+
+    private String nome;
+    
+    public static void main(String[] args) {
+
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String dizerOla(){
+        return "Ola";
+    }
+
+    
+}
+
+
